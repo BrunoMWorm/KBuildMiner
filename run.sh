ffdd833da37d04125fe897e05a3cac10158f782a
Christian Kaestner <kaestner@cs.cmu.edu>
2016-07-01 15:56:25 -0400

    increase stack size

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 BASEDIR=$(dirname "$0")
-java -ea -Xmx2G -Xms128m -Xss20m -classpath "$BASEDIR/target/classes:$(cat $BASEDIR/.classpath-scala)" "$@"
+java -ea -Xmx2G -Xms128m -Xss50m -classpath "$BASEDIR/target/classes:$(cat $BASEDIR/.classpath-scala)" "$@"
